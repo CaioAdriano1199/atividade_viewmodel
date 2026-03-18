47105a6e9cb7ddb48af42c53d017a9e61838ad07
CaioAdriano1199 <caioadriano1199@gmail.com>
2026-03-17 21:28:30 -0300

    data formatada

---
+++ app/src/main/java/model/Task.kt
@@ -0,0 +1,21 @@
+package model
+
+import java.time.LocalDateTime
+
+data class Task (
+    val title: String= "",
+    val status: Status= Status.NotDone,
+    val priority: Priority= Priority.Low,
+    val dateTime: LocalDateTime = LocalDateTime.now()
+)
+
+enum class Status(){
+    Done,
+    NotDone
+}
+
+enum class Priority(){
+    Low,
+    Medium,
+    High
+}
